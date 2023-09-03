326a8a4c62f39ef4eca2e18b84fb1a9a83b7f42d
Piotr Dros <piotr.dros01@gmail.com>
2023-09-03 11:48:18 +0200

    chore: add react eslint plugin extenstion

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -4,12 +4,14 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
